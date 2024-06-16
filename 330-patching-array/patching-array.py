000657bb0ce11a86b84f892dee52d76374993a8e
𝚂𝚊𝚒 𝙺𝚛𝚒𝚜𝚑𝚗𝚊🌊 <152096539+Techy7795@users.noreply.github.com>
2024-06-16 23:56:51 +0530

    Time: 61 ms (31.12%) | Memory: 16.7 MB (69.93%) - LeetSync

---
+++ 330-patching-array/patching-array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minPatches(self, nums: List[int], n: int) -> int:
+        miss = 1
+        count = 0
+        i = 0
+        while miss <= n:
+            if i < len(nums) and nums[i] <= miss:
+                miss += nums[i]
+                i += 1
+            else:
+                miss += miss
+                count += 1
+        return count
