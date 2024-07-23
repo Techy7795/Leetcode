f3bf2532e8515eea4d74ba787c8a75b219c133a7
𝚂𝚊𝚒 𝙺𝚛𝚒𝚜𝚑𝚗𝚊🌊 <152096539+Techy7795@users.noreply.github.com>
2024-07-23 14:23:11 +0530

    Time: 47 ms (75.86%) | Memory: 16.7 MB (12.04%) - LeetSync

--- 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.py
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.py
@@ -4,13 +4,4 @@ class Solution:
         c=Counter(nums)
         nums.sort(key = lambda x: (c[x],-x))
         return nums
-        # l=[]
-        # for i in c.keys():
-        #     l.append([i,c[i]])
-        # res=sorted(l,key=lambda x: x[1])
-        # print(res)
-        # ures=[]
-        # for i in res:
-        #     for j in range(i[1]):
-        #         ures.append(i[0])
-        # return ures
+        
