d4d40b254a9347cf0ae2eda8e6a9062d0cd71415
𝚂𝚊𝚒 𝙺𝚛𝚒𝚜𝚑𝚗𝚊🌊 <152096539+Techy7795@users.noreply.github.com>
2024-07-24 16:23:46 +0530

    Time: 627 ms (100.00%) | Memory: 23.1 MB (75.00%) - LeetSync

--- 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.py
+++ 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.py
@@ -1,21 +1,22 @@
 class Solution:
     def sortJumbled(self, mapping: List[int], nums: List[int]) -> List[int]:
-        mapped_nums={}
-        res=[]
-        z=""
-        for i in mapping:
-            z+=str(i)
-        for i in nums:
-            s=str(i)
-            trans=str.maketrans("0123456789",z)
-            c=int(s.translate(trans))
-            if c not in mapped_nums:
-                mapped_nums[c]=[i]
-            else:
-                mapped_nums[c].append(i)
-        # print(mapped_nums)
-        for i in sorted(mapped_nums):
-            for j in mapped_nums[i]:
-                res.append(j)
-        return res
-        
+        # mapped_nums={}
+        # res=[]
+        # z=""
+        # for i in mapping:
+        #     z+=str(i)
+        # for i in nums:
+        #     s=str(i)
+        #     trans=str.maketrans("0123456789",z)
+        #     c=int(s.translate(trans))
+        #     if c not in mapped_nums:
+        #         mapped_nums[c]=[i]
+        #     else:
+        #         mapped_nums[c].append(i)
+        # # print(mapped_nums)
+        # for i in sorted(mapped_nums):
+        #     for j in mapped_nums[i]:
+        #         res.append(j)
+        # return res
+        trans_rule = str.maketrans({str(i):str(x) for i,x in enumerate(mapping)})
+        return sorted(nums, key=lambda x: int(str(x).translate(trans_rule)))
