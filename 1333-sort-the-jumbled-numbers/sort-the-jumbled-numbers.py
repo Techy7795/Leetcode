34b660000e78bef6bc4d0c795b0de8d3d829fb25
𝚂𝚊𝚒 𝙺𝚛𝚒𝚜𝚑𝚗𝚊🌊 <152096539+Techy7795@users.noreply.github.com>
2024-07-24 16:16:46 +0530

    Time: 852 ms (90.76%) | Memory: 26.3 MB (23.91%) - LeetSync

---
+++ 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.py
@@ -0,0 +1,21 @@
+class Solution:
+    def sortJumbled(self, mapping: List[int], nums: List[int]) -> List[int]:
+        mapped_nums={}
+        res=[]
+        z=""
+        for i in mapping:
+            z+=str(i)
+        for i in nums:
+            s=str(i)
+            trans=str.maketrans("0123456789",z)
+            c=int(s.translate(trans))
+            if c not in mapped_nums:
+                mapped_nums[c]=[i]
+            else:
+                mapped_nums[c].append(i)
+        # print(mapped_nums)
+        for i in sorted(mapped_nums):
+            for j in mapped_nums[i]:
+                res.append(j)
+        return res
+        
