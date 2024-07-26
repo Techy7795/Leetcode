c5274285aa87e31b8eee327dc3de724eee5cd9d8
𝚂𝚊𝚒 𝙺𝚛𝚒𝚜𝚑𝚗𝚊🌊 <152096539+Techy7795@users.noreply.github.com>
2024-07-27 01:17:54 +0530

    Time: 161 ms (58.22%) | Memory: 17.2 MB (40.40%) - LeetSync

--- 1843-number-of-rectangles-that-can-form-the-largest-square/number-of-rectangles-that-can-form-the-largest-square.py
+++ 1843-number-of-rectangles-that-can-form-the-largest-square/number-of-rectangles-that-can-form-the-largest-square.py
@@ -1,10 +1,4 @@
 class Solution:
     def countGoodRectangles(self, r: List[List[int]]) -> int:
-        d={}
-        for i in r:
-            if min(i) in d:
-                d[min(i)]+=1
-            else:
-                d[min(i)]=1
-        return d[max(d)]
-        
+        l=[min(i) for i in r]
+        return l.count(max(l))
