a0cd622a1ae9be06403243faa060f5543844796f
𝚂𝚊𝚒 𝙺𝚛𝚒𝚜𝚑𝚗𝚊🌊 <152096539+Techy7795@users.noreply.github.com>
2024-07-30 20:37:10 +0530

    Time: 60 ms (99.77%) | Memory: 17.1 MB (69.50%) - LeetSync

--- 962-flip-string-to-monotone-increasing/flip-string-to-monotone-increasing.py
+++ 962-flip-string-to-monotone-increasing/flip-string-to-monotone-increasing.py
@@ -9,16 +9,16 @@ class Solution:
                 count -= 1
         return ans
 
-        acr=0
-        for i in s:
-            acr+=1 if i=="0" else 0
-        bcl=0
-        res=len(s)
-        for c in s:
-            if c=="0":
-                acr-=1
-            res=min(res,acr+bcl)
-            if c=="1":
-                bcl+=1 
-        return res
+        # acr=0
+        # for i in s:
+        #     acr+=1 if i=="0" else 0
+        # bcl=0
+        # res=len(s)
+        # for c in s:
+        #     if c=="0":
+        #         acr-=1
+        #     res=min(res,acr+bcl)
+        #     if c=="1":
+        #         bcl+=1 
+        # return res
         
