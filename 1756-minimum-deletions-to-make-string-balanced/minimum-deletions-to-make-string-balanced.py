307acb8e77c096e5ddc6957eaa2015512dd3bd44
𝚂𝚊𝚒 𝙺𝚛𝚒𝚜𝚑𝚗𝚊🌊 <152096539+Techy7795@users.noreply.github.com>
2024-07-30 19:48:25 +0530

    Time: 160 ms (95.00%) | Memory: 17.6 MB (79.00%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.py
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.py
@@ -1,16 +1,24 @@
 class Solution:
     def minimumDeletions(self, s: str) -> int:
-        acr=0
+        ans, count = 0, 0
         for i in s:
-            acr+=1 if i=="a" else 0
-        bcl=0
-        res=len(s)
-        for c in s:
-            if c=="a":
-                acr-=1
-            res=min(res,acr+bcl)
-            if c=="b":
-                bcl+=1 
-        return res
+            if i == 'b':
+                count += 1
+            elif count:
+                ans += 1
+                count -= 1
+        return ans
+        # acr=0
+        # for i in s:
+        #     acr+=1 if i=="a" else 0
+        # bcl=0
+        # res=len(s)
+        # for c in s:
+        #     if c=="a":
+        #         acr-=1
+        #     res=min(res,acr+bcl)
+        #     if c=="b":
+        #         bcl+=1 
+        # return res
 
         
