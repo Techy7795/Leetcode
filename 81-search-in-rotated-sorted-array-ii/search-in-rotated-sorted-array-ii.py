ad7767951ef3bbe17d53c6c766a5f5422b42de41
𝚂𝚊𝚒 𝙺𝚛𝚒𝚜𝚑𝚗𝚊🌊 <152096539+Techy7795@users.noreply.github.com>
2024-07-31 20:31:31 +0530

    Time: 58 ms (20.91%) | Memory: 17 MB (95.21%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
@@ -1,4 +1,38 @@
 class Solution:
-    def search(self, nums: List[int], target: int) -> bool:
+    def search(self, arr: List[int], k: int) -> bool:
+        n = len(arr)  # size of the array
+        low, high = 0, n - 1
+
+        while low <= high:
+            mid = (low + high) // 2
+
+            # if mid points to the target
+            if arr[mid] == k:
+                return True
+
+            # Edge case:
+            if arr[low] == arr[mid] and arr[mid] == arr[high]:
+                low += 1
+                high -= 1
+                continue
+
+            # if left part is sorted
+            if arr[low] <= arr[mid]:
+                if arr[low] <= k <= arr[mid]:
+                    # element exists
+                    high = mid - 1
+                else:
+                    # element does not exist
+                    low = mid + 1
+            else:  # if right part is sorted
+                if arr[mid] <= k <= arr[high]:
+                    # element exists
+                    low = mid + 1
+                else:
+                    # element does not exist
+                    high = mid - 1
+
+        return False
+
         return target in nums
         
