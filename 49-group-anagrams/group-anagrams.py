cfbadc73b9558e6902b1d3cdc0761b4daf1423b0
𝚂𝚊𝚒 𝙺𝚛𝚒𝚜𝚑𝚗𝚊🌊 <152096539+Techy7795@users.noreply.github.com>
2024-08-01 20:01:59 +0530

    Time: 85 ms (78.12%) | Memory: 20.7 MB (36.88%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.py
@@ -0,0 +1,12 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        ans={}
+        for s in strs:
+            s_sorted=sorted(s)
+            key=tuple(s_sorted)
+            if key not in ans:
+                ans[key]=[s]
+            else:
+                ans[key].append(s)
+        return ans.values()
+            
