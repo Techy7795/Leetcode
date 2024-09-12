a4d94b13978e86a2d82358bb9b82e9044c09f5ad
𝚂𝚊𝚒 𝙺𝚛𝚒𝚜𝚑𝚗𝚊🌊 <152096539+Techy7795@users.noreply.github.com>
2024-09-12 17:23:01 +0530

    Time: 40 ms (19.93%) | Memory: 16.6 MB (23.56%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,7 +1,9 @@
 class Solution:
     def strStr(self, h: str, n: str) -> int:
-        if n in h:
-            return h.index(n)
-        else:
-            return -1
+        hlen=len(h)
+        nlen=len(n)
+        for i in range((hlen-nlen)+1):
+            if h[i:i+nlen]==n:
+                return i
+        return -1
         
